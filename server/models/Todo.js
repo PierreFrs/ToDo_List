3e45c9aa69225b6f0cb2bf166b04c47fd788124b
Pierre <pierrefraisse90@gmail.com>
2023-04-27 17:29:24 +0100

    UI/backend

---
+++ server/models/Todo.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Creates a new schema
+const TodoSchema = new Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    complete: {
+        type: Boolean,
+        default: false
+    },
+    timestamp: {
+        type: String,
+        default: Date.now()
+    }
+});
+
+const Todo = mongoose.model("Todo", TodoSchema);
+
+module.exports = Todo;
